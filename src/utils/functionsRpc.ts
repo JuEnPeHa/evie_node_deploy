8f557f593e4cdcb23bd91a99bee9c3834873cab0
JuEnPeHa <67208274+JuEnPeHa@users.noreply.github.com>
2022-07-06 18:24:04 -0500

    4

--- src/utils/functionsRpc.ts
+++ src/utils/functionsRpc.ts
@@ -24,7 +24,9 @@ export function getMarketplacesClean(listNftMarketplacesRaw: string[]): string[]
             if (marketplace.includes("mintbase") ||
             marketplace.includes("paras") ||
             marketplace.includes("neatar")  ||
-            marketplace.includes("mintspace") ){
+            marketplace.includes("mintspace") ||
+            marketplace.includes("higgsfield")
+            ){
                 listNftMarketplaces.push(marketplace);
             }
         }
@@ -124,8 +126,12 @@ export async function getLandingPageHiggsFieldPrivate(
 ) {
     const { data } = await higgsfieldAPI.post<HiggsfieldCollectionResponse>('/',
     {
-        
-    })
+        "next_id": nextId,
+        "limit": limit,
+        "days": days,
+        "name": name
+    });
+    console.log("data :" + data);
 }
 
 export async function getLandingPageMintbasePrivate(limit: number) {
